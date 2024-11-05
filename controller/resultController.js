034b21f07b604fe6c0d3cea9da53b6cccf2bda0c
kushdeepwalia <skushdeep5@gmail.com>
2024-11-05 19:29:31 +0530

    resolved error

--- controller/resultController.js
+++ controller/resultController.js
@@ -26,9 +26,8 @@ async function submitAssessment(req, res, next) {
 
 async function getAll(req, res, next) {
    try {
-      const result = await Result.find().lean().populate('userId', 'name rollno awcentre age').populate('questions.quesId', 'question.correctAnswer question.questionType').populate('questions.quesCategory', 'categoryName');
-      console.log(result[0].questions[0]);
-      if (result.length === 0) {
+      const result = await Result.countDocuments() !== 0 ? await Result.find().lean().populate('userId', 'name rollno awcentre age').populate('questions.quesId', 'question.correctAnswer question.questionType').populate('questions.quesCategory', 'categoryName') : 0;
+      if (!result || result.length === 0) {
          res.status(201).json({
             message: "No Record Found"
          })
