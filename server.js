86bdc9f46f39c0323e9183809313d4dc1a89e1dc
kushdeepwalia <skushdeep5@gmail.com>
2024-12-05 23:33:29 +0530

    database connected successfully

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ app.listen(process.env.PORT || 5000, function () {
       this.address().port,
       app.settings.env
    );
-   connection("mongodb+srv://divinelabcsc:SdQZ3dKkp1m2AITl@cscAnganwadiI2.dmjpu.mongodb.net/?retryWrites=true&w=majority&appName=csctabapp");
+   connection("mongodb+srv://divinelabcsc:SdQZ3dKkp1m2AITl@cscanganwadii2.d6mu5.mongodb.net/?retryWrites=true&w=majority&appName=csctabapp");
 });
