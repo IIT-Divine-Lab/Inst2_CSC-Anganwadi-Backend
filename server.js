4969b64e17c27efbf981c1c9ab02d354ef34bb9d
kushdeepwalia <skushdeep5@gmail.com>
2025-02-20 19:05:06 +0530

    Changes Made

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const cors = require("cors");
 const connection = require("./startup/db");
 const app = express();
 const userRoutes = require("./routes/student")
